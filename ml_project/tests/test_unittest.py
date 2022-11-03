b8912aa2a92e297482d9b97f91a553578d7bdd85
Dmitrii <dkorzh10@gmail.com>
2022-11-03 21:43:13 +0300

    tests: added new

--- ml_project/tests/test_unittest.py
+++ ml_project/tests/test_unittest.py
@@ -1,7 +1,31 @@
 import unittest
 from unittest.mock import patch
+import src
 import os
+import numpy as np
+import pandas as pd
+from numpy.random import randint
 
+from src.data import read_data, split_train_val_data
+from src.entities.predict_pipeline_params import (
+    PredictPipelineParams,
+    read_predict_pipeline_params,
+)
+from src.entities.train_pipeline_params import (
+    TrainingPipelineParams,
+    read_training_pipeline_params,
+)
+
+from src.features import make_features
+from src.features.build_features import extract_target, build_transformer
+from src.models import (
+    train_model,
+    serialize_model,
+    predict_model,
+    evaluate_model,
+)
+
+from src.models.model_fit_predict import create_inference_pipeline
 
 class DoesntExists(Exception):
     print('File doesnt exist')
@@ -15,164 +39,138 @@ class TestProject(unittest.TestCase):
         else:
             raise DoesntExists
 
+    def test_predict(self):
+        config_path = "config/predict_config_logreg.yaml"
+        predict_pipeline_params = read_predict_pipeline_params(config_path)
+        _, outputs = src.predict.run_predict_pipeline(predict_pipeline_params)
+        # print(len(outputs))
 
-    # @patch('builtins.input', side_effect=['2 3'])
-    # def test_validate_input1(self, _):
-    #     '''
-    #     Validation of correct input
-    #     '''
-    #     game = TicTacGame()
-    #     x, y = game.validate_input()
-    #     self.assertEqual([x, y], [2, 3])
+        self.assertEqual(len(outputs), 297)
+        self.assertEqual(isinstance(outputs, np.ndarray), True)
+    
+    def test_predict_on_random_data(self):
+        config_path = "config/predict_config_logreg.yaml"
+        predict_pipeline_params = read_predict_pipeline_params(config_path)
+        columns = [
+            "age", "sex", "cp", "trestbps", "chol", "fbs",
+            "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"
+            ]
+        def generate_data():
+            data = [
+                randint(50, 70), randint(0, 1), randint(0, 3), randint(100, 180),
+                randint(50, 350), randint(0, 1), randint(0, 2), 
+                randint(90, 170), randint(0, 1), randint(0, 9),
+                randint(0, 2), randint(0, 3), randint(0, 2)
+                ]
+            return data
 
-    # @patch('builtins.input', side_effect=['-10 1'])
-    # def test_validate_input2(self, _):
-    #     '''
-    #     Validation of incorrect input,
-    #     when wrong indexes were given
-    #     '''
-    #     game = TicTacGame()
-    #     with self.assertRaises(IndexError):
-    #         game.validate_input()
 
-    # @patch('builtins.input', side_effect=['1 2'])
-    # def test_validate_input3(self, _):
-    #     '''
-    #     Validation of incorrect input,
-    #     when the indexes were given of already taken position
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', 'X'],
-    #         ['0', '0', ' '],
-    #         [' ', ' ', ' ']
-    #     ]
-    #     with self.assertRaises(TakenError):
-    #         game.validate_input()
+        data = generate_data() 
+        df = pd.DataFrame([data], columns=columns)
+        df.to_csv('data/random_line.csv', index=False)
+        predict_pipeline_params.path_to_data = 'data/random_line.csv'
+        _, outputs = src.predict.run_predict_pipeline(predict_pipeline_params)
 
-    # @patch('builtins.input', side_effect=['1 100'])
-    # def test_validate_input4(self, _):
-    #     '''
-    #     Validation of incorrect input,
-    #     when wrong indexes (out of box) were given
-    #     '''
-    #     game = TicTacGame()
-    #     with self.assertRaises(IndexError):
-    #         game.validate_input()
+        self.assertEqual(len(outputs), 1)
+        self.assertEqual((outputs in [0, 1]), True)
+        self.assertEqual(isinstance(outputs, np.ndarray), True)
+    
+    # def test_train_acc(self, acc):
+    #     return self.assertGreater(acc, 0.0)
 
-    # @patch('builtins.input', side_effect=['1 2 i want win'])
-    # def test_validate_input5(self, _):
-    #     '''
-    #     Validation of incorrect input,
-    #     when wrong input was given
-    #     '''
-    #     game = TicTacGame()
-    #     with self.assertRaises(ValueError):
-    #         game.validate_input()
+    def test_training_procedure(self):
+        config_path = "config/train_config_logreg.yaml"
+        training_pipeline_params = read_training_pipeline_params(config_path)
+        columns = [
+            "age", "sex", "cp", "trestbps", "chol", "fbs",
+            "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal",
+            "condition"
+            ]
+        def generate_data():
+            data = [
+                randint(50, 70), randint(0, 1), randint(0, 3), randint(100, 180),
+                randint(50, 350), randint(0, 1), randint(0, 2), 
+                randint(90, 170), randint(0, 1), randint(0, 9),
+                randint(0, 2), randint(0, 3), randint(0, 2),
+                randint(0, 1)
+                ]
+            return data
+        data = [generate_data() for i in range(100)]
+        df = pd.DataFrame(np.array(data), columns=columns)
+        df.to_csv('data/random_train.csv', index=False)
+        training_pipeline_params.path_to_data = 'data/random_trine.csv'
 
-    # def test_check_winner1(self):
-    #     '''
-    #     Checking that X has won (row)
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', 'X'],
-    #         ['0', '0', ' '],
-    #         [' ', ' ', ' ']
-    #     ]
-    #     self.assertEqual(game.check_winner(), 'X')
+        data = read_data(training_pipeline_params.input_data_path)
+        train_df, val_df = split_train_val_data(
+            data, training_pipeline_params.splitting_params
+        )
+        b = (len(val_df) / len(data))
+        b1 = (b < training_pipeline_params.splitting_params.val_size * 1.1)
+        b2 = (b > training_pipeline_params.splitting_params.val_size * 0.9)
+        self.assertEqual((b1 and b2), True)
 
-    # def test_check_winner2(self):
-    #     '''
-    #     Checking that 0 has won (diagonal)
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', '0'],
-    #         ['0', '0', 'X'],
-    #         ['0', 'X', ' ']
-    #     ]
-    #     self.assertEqual(game.check_winner(), '0')
+        val_target = extract_target(val_df, training_pipeline_params.feature_params)
 
-    # def test_step1(self):
-    #     '''
-    #     Checking the correct state of the board
-    #     after 0 makes a step
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', ' '],
-    #         ['0', '0', ' '],
-    #         [' ', 'X', ' ']
-    #     ]
-    #     game.sign = '0'
-    #     expected = [
-    #         ['X', 'X', ' '],
-    #         ['0', '0', '0'],
-    #         [' ', 'X', ' ']
-    #     ]
-    #     game.make_step(2, 3)
-    #     self.assertEqual(game.board, expected)
+        self.assertEqual(val_target.shape, (60,))
+        train_target = extract_target(train_df, training_pipeline_params.feature_params)
+        train_df = train_df.drop(training_pipeline_params.feature_params.target_col, 1)
+        val_df = val_df.drop(training_pipeline_params.feature_params.target_col, 1)
 
-    # def test_step2(self):
-    #     '''
-    #     Checking the correct state of the board
-    #     after X makes a step
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', ' '],
-    #         ['0', '0', ' '],
-    #         [' ', ' ', ' ']
-    #     ]
-    #     game.sign = 'X'
-    #     expected = [
-    #         ['X', 'X', ' '],
-    #         ['0', '0', ' '],
-    #         [' ', 'X', ' ']
-    #     ]
-    #     game.make_step(3, 2)
-    #     self.assertEqual(game.board, expected)
+        transformer = build_transformer(training_pipeline_params.feature_params)
+        transformer.fit(train_df)
+        train_features = make_features(transformer, train_df)
+        # print(train_features)
 
-    # def test_check_winner3(self):
-    #     '''
-    #     Checking that X has won
-    #     (second diagonal)
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', ' ', '0'],
-    #         [' ', 'X', '0'],
-    #         [' ', ' ', 'X']
-    #     ]
-    #     self.assertEqual(game.check_winner(), 'X')
+        model = train_model(
+            train_features, train_target, training_pipeline_params.train_params
+        )
 
-    # def test_check_winner4(self):
-    #     '''
-    #     Checking that 0 has won
-    #     (column)
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', 'X', '0'],
-    #         ['X', ' ', '0'],
-    #         [' ', ' ', '0']
-    #     ]
-    #     self.assertEqual(game.check_winner(), '0')
+        inference_pipeline = create_inference_pipeline(model, transformer)
+        predicts = predict_model(
+            inference_pipeline,
+            val_df,
+            training_pipeline_params.feature_params.use_log_trick,
+        )
+        metrics = evaluate_model(
+            predicts,
+            val_target,
+            use_log_trick=False,
+            model_name=training_pipeline_params.train_params.model_type
+        )
+        self.assertGreater(metrics['accuracy'], 0.0)
+        self.assertLessEqual(metrics['accuracy'], 1.0)
 
-    # def test_check_winner5(self):
-    #     '''
-    #     Checking that nobody has won
-    #     (draw)
-    #     '''
-    #     game = TicTacGame()
-    #     game.board = [
-    #         ['X', '0', 'X'],
-    #         ['X', 'X', '0'],
-    #         ['0', 'X', '0']
-    #     ]
-    #     self.assertEqual(game.check_winner(), 0)
+    def test_transformer(self):
+        config_path = "config/train_config_logreg.yaml"
+        training_pipeline_params = read_training_pipeline_params(config_path)
+        columns = training_pipeline_params.feature_params.numerical_features
+
+        data = read_data(training_pipeline_params.input_data_path)
+        full_cols = columns + [training_pipeline_params.feature_params.target_col]
+        data = data[full_cols]
+        train_df, val_df = split_train_val_data(
+            data, training_pipeline_params.splitting_params
+        )
+
+        training_pipeline_params.feature_params.categorical_features = []
+       
+        val_target = extract_target(val_df, training_pipeline_params.feature_params)
+        train_target = extract_target(train_df, training_pipeline_params.feature_params)
+        train_df = train_df.drop(training_pipeline_params.feature_params.target_col, 1)
+        val_df = val_df.drop(training_pipeline_params.feature_params.target_col, 1)
+
+        transformer = build_transformer(training_pipeline_params.feature_params)
+        transformer.fit(train_df)
+        train_features = make_features(transformer, train_df)
+        # print(train_features)
+
+        self.assertGreater(train_features.mean(), -0.3)
+        self.assertLess(train_features.mean(), 0.3)
+
+        self.assertGreater(train_features[0].std(), -1.3)
+        self.assertLess(train_features[0].std(), 1.3)
 
 
 if __name__ == '__main__':
     unittest.main()
+
